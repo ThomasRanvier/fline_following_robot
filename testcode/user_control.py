789449b35f13a5eabc23e4f57f203a01e7ddf81a
infiniityr <valentin.lecompte@gmail.com>
2018-12-07 13:44:42 +0100

    modif user_control

--- testcode/user_control.py
+++ testcode/user_control.py
@@ -1,19 +1,67 @@
 from bbio import *
 from simple_pid import PID
 
-RED_LED = GPIO1_7
-pid = PID(1,00726, 17.38404, 0, setpoint=0)
-current_value = 0
+MOTOR1 = PWM2B
+pid = PID(1.00726, 18.38404, 0)
+pause = 10
+counter = 0.0
+value = 0
+
+step_error = 0.0
+
+Kp = 1.00726
+Ki = 18.38404
+pi_value = 0.0
+ISum = 0.0
+
+def encoder(PIN1, PIN2):
+    pinMode(PIN1, INPUT)
+    pinMode(PIN2, INPUT)
+    attachInterrupt(PIN1, increment_encoder)
+    attachInterrupt(PIN2, increment_encoder)
+
+
+def increment_encoder():
+    global counter
+    counter += 1.0
+
+
+def pi_controller(e):
+    global ISum
+    ISum += e * 0.01
+    u = Kp * e + Ki * ISum
+
+    if u > 255:
+        return 255
+    elif u < 0:
+        return 0
+    else:
+        return u
+
 
 def setup():
-    pinMode(RED_LED, OUTPUT)
-    
-def loop():
+    global value
     value = input('Enter a number (0-255): ')
-    pid.setpoint(value)
-    while True:
-        new_value = pid(current_value)
-        pwmWrite(RED_LED, new_value, resolution=RES_8BIT)
-        current_value = new_value
-    
+    encoder(PIN1=GPIO2_8, PIN2=GPIO2_9)
+    pid.setpoint = value
+    pid.output_limits = (0, 255)
+    pass
+
+
+def loop():
+    global pid, value, counter, step_error, pi_value
+
+    pid.setpoint = value
+
+    print "counter : " + str(counter / 0.01)
+    step_error = value - (counter / 0.01 * 255 / 8000)
+    counter = 0.0
+
+    pi_value = pi_controller(step_error)
+    print "step_error : " + str(step_error)
+    print "pid : " + str(pi_value)
+    analogWrite(MOTOR1, pi_value, resolution=RES_8BIT)
+
+    delay(pause)
+
 run(setup, loop)
